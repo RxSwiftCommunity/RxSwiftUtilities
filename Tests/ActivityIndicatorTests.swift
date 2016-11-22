733d433c3ebe1450752f3d0486afa8f20c83448e
Jesse Farless <solidcell@gmail.com>
2016-11-22 09:44:50 +0100

    Add a test for a single tracking

--- Tests/ActivityIndicatorTests.swift
+++ Tests/ActivityIndicatorTests.swift
@@ -8,15 +8,28 @@
 
 import XCTest
 import RxSwiftUtilities
+import RxSwift
 
 class ActivityIndicatorTests: XCTestCase {
-    
+
     func testInitiallyEmitsFalse() {
         let activityIndicator = ActivityIndicator()
         var initialValue = true
         let _ = activityIndicator.asObservable()
             .subscribe(onNext: { initialValue = $0 })
         XCTAssertFalse(initialValue)
+    }
+
+    func testEmitsTrueWhenTracking() {
+        let activityIndicator = ActivityIndicator()
+        var value = false
+        let _ = activityIndicator.asObservable()
+            .subscribe(onNext: { value = $0 })
+        let _ = Observable.just(1)
+            .concat(Observable.never())
+            .trackActivity(activityIndicator)
+            .subscribe()
+        XCTAssertTrue(value)
     }
     
 }
