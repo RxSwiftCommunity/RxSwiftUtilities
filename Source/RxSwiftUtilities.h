dd8fff383ed1a21ef37ed544b69dfb23226d75b6
Jesse Farless <solidcell@gmail.com>
2016-11-22 15:38:12 +0100

    Add macOS framework

--- Source/RxSwiftUtilities.h
+++ Source/RxSwiftUtilities.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 solidcell. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for RxSwiftUtilities.
 FOUNDATION_EXPORT double RxSwiftUtilitiesVersionNumber;
