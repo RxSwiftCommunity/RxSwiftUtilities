31e114120881963af09e011989089c93f1328800
Jesse Farless <solidcell@gmail.com>
2016-11-22 16:13:16 +0100

    Replace initial view controller with an empty static table

---
+++ ExampleApp/ExampleApp/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Jesse Farless on 11/22/16.
+//  Copyright © 2016 solidcell. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
