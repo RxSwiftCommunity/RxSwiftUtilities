2fc9e485e125a73ccf1e4bb0e76a1f820f98d928
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2018-03-09 00:55:25 +0900

    Migrate the code to RxSwift 4

--- Source/Operators.swift
+++ Source/Operators.swift
@@ -37,7 +37,7 @@ public func nonMarkedText(_ textInput: UITextInput) -> String? {
 
 public func <-> <Base>(textInput: TextInput<Base>, variable: Variable<String>) -> Disposable {
     let bindToUIDisposable = variable.asObservable()
-        .bindTo(textInput.text)
+        .bind(to: textInput.text)
     let bindToVariable = textInput.text
         .subscribe(onNext: { [weak base = textInput.base] n in
             guard let base = base else {
@@ -81,7 +81,7 @@ public func <-> <T>(property: ControlProperty<T>, variable: Variable<T>) -> Disp
      */
     
     let bindToUIDisposable = variable.asObservable()
-        .bindTo(property)
+        .bind(to: property)
     let bindToVariable = property
         .subscribe(onNext: { n in
             variable.value = n
@@ -93,5 +93,3 @@ public func <-> <T>(property: ControlProperty<T>, variable: Variable<T>) -> Disp
 }
 
 // }
-
-
