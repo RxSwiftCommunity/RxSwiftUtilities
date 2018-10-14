fd467098013b73c2676a1580f58e4dc535188f56
Jesse Farless <solidcell@gmail.com>
2018-10-14 11:55:49 +0200

    Xcode project updates

--- ExampleApp/ExampleApp/TwoWayBindingViewController.swift
+++ ExampleApp/ExampleApp/TwoWayBindingViewController.swift
@@ -32,7 +32,7 @@ class TwoWayBindingViewController: UITableViewController, UITextFieldDelegate {
         textField.delegate = self
         textField.returnKeyType = UIReturnKeyType.done
 
-        NotificationCenter.default.rx.notification(Notification.Name.UIKeyboardDidShow)
+        NotificationCenter.default.rx.notification(UIResponder.keyboardDidShowNotification)
             .subscribe(onNext: { _ in
                 var rect = tableView.convert(stackView.frame, from: stackView)
                 rect = CGRect(x: rect.minX, y: rect.minY + 20, width: rect.width, height: rect.height)
@@ -51,8 +51,8 @@ class TwoWayBindingViewController: UITableViewController, UITextFieldDelegate {
         stackView.distribution = .equalCentering
 
         ["🦆", "🥐", "🚒", "🍻", "🌻"].forEach { text in
-            let button = UIButton(type: UIButtonType.system)
-            button.setTitle(text, for: UIControlState.normal)
+            let button = UIButton(type: UIButton.ButtonType.system)
+            button.setTitle(text, for: UIControl.State.normal)
             stackView.addArrangedSubview(button)
             button.rx.tap
                 .asDriver()
